5a9183c0943ef11dd52c4dc8a06405d9ec9bf3df
Micah Brown <micahgbrown@gmail.com>
2026-07-03 00:25:08 +0100

    Fixed typing issue

--- Custom-SPDE/simulators.py
+++ Custom-SPDE/simulators.py
@@ -9,14 +9,14 @@ from collections.abc import Generator, Sequence
 def run_simulation_semi_implicit(dt: float, 
                    t_range: tuple[float, float], 
                    initial_state: np.ndarray, 
-                   diffusion_matrix: scipy.sparse.coo_matrix, 
+                   diffusion_matrix: scipy.sparse.csr_matrix, 
                    reaction_fn: Callable[[np.typing.NDArray[np.float32], float], np.typing.NDArray[np.float32]],
                    call_every: tuple[int, Callable[[float, np.typing.NDArray[np.float32]], None]]):
     """Run simulation using Euler-Maruyama, with the diffusion solved using an implicit step"""
 
     call_interval, call = call_every
 
-    I = scipy.sparse.eye(diffusion_matrix.shape[0])
+    I = scipy.sparse.eye(diffusion_matrix.shape[0]) # type: ignore
     lhs = I - dt * diffusion_matrix
     solver = spla.factorized(lhs.tocsc())
 
